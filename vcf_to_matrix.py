246766e8f7da1526e257bf46881e9ca00dfb9527
Rajesh Moturu <rajeshmoturu2@gmail.com>
2022-08-26 01:09:17 +0200

    Add progress bar to show file read percent

--- vcf_to_matrix.py
+++ vcf_to_matrix.py
@@ -7,11 +7,14 @@ import numpy as np
 from pysam import VariantFile
 import pandas as pd
 
+from utils import start_progress, progress, end_progress
+
 
 def read_input_files(args) -> None:
     sample, genotypes, variant_ids = [], [], []
     counter = 0
     with VariantFile(args.vcf_file) as vcf_reader:
+        start_progress("\n% of vcf file read ")
         for record in vcf_reader:
             # if we consider all samples, it's resource expensive. so pick 1 record but for every 100 to avoid bias
             if counter % 100 == 0:
@@ -21,8 +24,10 @@ def read_input_files(args) -> None:
                 genotypes.append(alleles)
                 variant_ids.append(record.id)
             if counter % 4943 == 0:
-                print(f'{round(100 * counter / 494328)}')
+                percent = round(100 * counter / 494328)
+                progress(percent)
             counter += 1
+        end_progress()
 
     genotypes = np.array(genotypes)
     genotypes = np.count_nonzero(genotypes, axis=2)
@@ -37,4 +42,3 @@ def read_input_files(args) -> None:
     matrix = pd.DataFrame(genotypes, columns=variant_ids, index=samples)  # save a copy to use in collab for plot
     matrix['Population code'] = matrix.index.map(labels)
     matrix.to_csv("./data/matrix.csv")
-
